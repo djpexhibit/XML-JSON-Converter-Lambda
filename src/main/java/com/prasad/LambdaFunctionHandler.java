50dc33f9cf1768d13b8bdd8b5959d4d87cb580fc
prasadwa <prasad.wanigasinghe@ebuilder.com>
2018-06-21 14:01:39 +0530

    handling request

--- src/main/java/com/prasad/LambdaFunctionHandler.java
+++ src/main/java/com/prasad/LambdaFunctionHandler.java
@@ -4,15 +4,53 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.prasad.invoker.AbstractServiceInvoker;
+import com.prasad.util.InvokerUtil;
+import com.prasad.util.Request;
+import com.prasad.util.Response;
+
+
 
 public class LambdaFunctionHandler implements RequestStreamHandler {
+	
+	private static final Logger LOG = LogManager.getLogger(LambdaFunctionHandler.class);
+
+    private static final InvokerUtil INVOKER_UTIL = new InvokerUtil();
+
 
 	@Override
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-		// TODO Auto-generated method stub
 		
+		JsonParser parser = new JsonParser();
+        JsonObject inputObj;
+        
+        try {
+            inputObj = parser.parse(IOUtils.toString(input,"UTF-8")).getAsJsonObject();	
+            
+			final Request request = new Request(inputObj);
+			LOG.info("Request: " + request.getJsonObject());
+			
+			final AbstractServiceInvoker invoker= INVOKER_UTIL.getInvoker(request);
+			final Response response = invoker.invoke(request);
+
+			response.send(output, context);
+
+
+        } catch (Exception e) {
+        	LOG.info(e.toString());
+        }
+		
+		
+        
+        
 	}
 
 }
